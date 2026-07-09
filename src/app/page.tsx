60ac85d253c7dca6e42f30814b323c335f77ba93
luccas <lukasmedina1721@gmail.com>
2026-07-09 15:20:12 -0300

    remove Benefits and Testimonials sections from homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import Hero from "@/components/home/Hero";
 import Categories from "@/components/home/Categories";
 import FeaturedProducts from "@/components/home/FeaturedProducts";
-import Benefits from "@/components/home/Benefits";
 import BrandsCarousel from "@/components/home/BrandsCarousel";
-import Testimonials from "@/components/home/Testimonials";
 
 export default function Home() {
   return (
@@ -11,9 +9,7 @@ export default function Home() {
       <Hero />
       <Categories />
       <FeaturedProducts />
-      <Benefits />
       <BrandsCarousel />
-      <Testimonials />
     </>
   );
 }
